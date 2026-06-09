9ac0f19ba2612b54288c5835b7116d01318c1dc1
66nihaal44 <nihaalshetty171@gmail.com>
2026-06-08 20:41:04 -0500

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,5 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "http://placeholder.com"
 res = requests.get(URL)
-html = res.content
+html = res.text
+bsoup = BeautifulSoup(html)
+for a in bsoup.find_all('a', href=True):
+  # code to add a['href'] to tab
+  title = str(soup.find('title').string) or None;
